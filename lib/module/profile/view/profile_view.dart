d9a2a0711721216c65bcff1afb4909ef8b52f9e7
fanes-setiawan <fanessetiawan.1401@gmail.com>
2023-07-21 00:00:25 +0700

    create a sliverappbar on the profile view

--- lib/module/profile/view/profile_view.dart
+++ lib/module/profile/view/profile_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ovo/core.dart';
+import '../../home/widget/buttonImageHorizontal.dart';
+import '../../home/widget/carouselBanner.dart';
+import '../../home/widget/finansialCard.dart';
+import '../../home/widget/learnOvo.dart';
 import '../controller/profile_controller.dart';
 
 class ProfileView extends StatefulWidget {
@@ -9,17 +13,209 @@ class ProfileView extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Profile"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: const [],
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            automaticallyImplyLeading: false,
+            pinned: true,
+            snap: true,
+            floating: true,
+            expandedHeight: 100.0,
+            flexibleSpace: const FlexibleSpaceBar(
+              titlePadding: EdgeInsets.only(left: 15),
+              title: Text(
+                'Profile',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
-        ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                if (index == 0) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Colors.grey,
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      CircleAvatar(
+                                        radius: 28.0,
+                                        backgroundColor: AppColors.greyBg,
+                                        child: Icon(
+                                          Icons.account_circle,
+                                          size: 55,
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Fanes Setiawan",
+                                            style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          Text(
+                                            "0883-2435-XXXX",
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      "Ubah",
+                                      style: TextStyle(
+                                        color: AppColors.blueLightText,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: InkWell(
+                                  onTap: () {},
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      border: Border.all(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.qr_code,
+                                          size: 30,
+                                        ),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          "Qr Code",
+                                          style: TextStyle(
+                                            color: AppColors.blackText,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Expanded(
+                                child: InkWell(
+                                  onTap: () {},
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      border: Border.all(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.back_hand_rounded,
+                                          size: 30,
+                                        ),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          "Loyalty Code",
+                                          style: TextStyle(
+                                            color: AppColors.blackText,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                //   else if (index % 2 == 1) {
+                //     return Container(
+                //       width: MediaQuery.of(context).size.width,
+                //       height: 8,
+                //       color: AppColors.whiteButton,
+                //     );
+                //   } else {
+                //     int widgetIndex = (index ~/ 2) - 1;
+                //     switch (widgetIndex) {
+                //       case 0:
+                //         return CarouselBanner();
+                //       case 1:
+                //         return ButtonImageHorizontal();
+                //       case 2:
+                //         return FinancialCard();
+                //       case 3:
+                //         return LearnOvo();
+                //       default:
+                //         return SizedBox.shrink();
+                //     }
+                //   }
+              },
+              childCount: 9, // Number of widgets + separator containers
+            ),
+          ),
+        ],
       ),
     );
   }
